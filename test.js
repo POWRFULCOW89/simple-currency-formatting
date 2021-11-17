18ab95506d11e92063854f8da6b7f7edd31059a9
Diego Melo <diego_14melo@hotmail.com>
2021-11-17 10:47:07 -0600

    chore: cleanup

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-const { formatCurrency } = require("./index.js");
+// const { formatCurrency } = require("./index.js");
+const { formatCurrency } = require("simple-currency-formatting");
 
 console.log(formatCurrency(1004.3, 2, ".", ",", "$", "USD"));
