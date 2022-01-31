527baa69ceab26c42ddf6917697da155123f917f
Diego Melo <diego_14melo@hotmail.com>
2022-01-31 12:31:11 -0600

    fix: ci crash

---
+++ src/test/test.ts
@@ -0,0 +1,4 @@
+// import { formatCurrency } from "../../dist";
+// // const { formatCurrency } = require("simple-currency-formatting");
+
+// console.log(formatCurrency(1004.3, 2, ".", ",", "$", "USD"));
--- test/test.ts
+++
@@ -1,4 +0,0 @@
-import { formatCurrency } from "../dist/index.js";
-// const { formatCurrency } = require("simple-currency-formatting");
-
-console.log(formatCurrency(1004.3, 2, ".", ",", "$", "USD"));
